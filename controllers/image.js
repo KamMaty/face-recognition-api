2a2b7833ecf419c30898d2af09b0ed85a17b6fe6
Kamil Matyasik <matyasik.k@wp.pl>
2024-07-20 00:14:07 +0200

    Added endpoint imageurl, sending request to clarifyapi

--- controllers/image.js
+++ controllers/image.js
@@ -1,5 +1,60 @@
+const setupClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = "26beae7f3fb0456589f393a140418ea7";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "mutu";
+  const APP_ID = "face-recognition";
+  // Change these to whatever model and image URL you want to use
+  // const MODEL_ID = "face-detection";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+const handleApiCall = (req, res) => {
+  fetch(
+    "https://api.clarifai.com/v2/models/face-detection/outputs",
+    setupClarifaiRequestOptions(req.body.input)
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.outputs) {
+        res.json(data.outputs[0].data.regions);
+      } else {
+        res.status(400).json("unable to work with api");
+      }
+    })
+    .catch((err) => res.status(400).json("unable to work with api"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
+
   db("users")
     .where("id", "=", id)
     .increment("entries", 1)
@@ -10,4 +65,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
